7e759847e0b9fcafc6c313baf8d8667cc515e5b7
O KJ <O KJ>
2022-09-07 16:15:57 +0900

    오늘은 두번째시간

---
+++ 02analogclock-t/aclock.js
@@ -0,0 +1,22 @@
+var hourHands = document.querySelector('[data-hour-hand]')
+var minuteHand = document.querySelector('[data-minute-hand]')
+var secondHand = document.querySelector('[data-second-hand]')
+
+function setClock(){
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds()/60
+    const minutesRatio = currentDate.getMinutes() /60
+    const hoursRatio = currentDate.getHours() / 12
+
+    setRotation(hourHands,hoursRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(secondHand, secondsRatio)
+
+}
+
+function setRotation(element,rotationRatio){
+    element.style.setproperty('--rotation',rotationRatio * 360)
+}
+
+setClock()
+setInterval(setClock,1000)
